1f34594d56be0507037672b3b1746f5aad70a3bf
Helsis <seagull.bogdan@gmail.com>
2019-09-21 23:47:53 +0300

    feat( api ): add database module

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { ConfigModule } from '../config/config.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
---
+++ server/src/database/database.module.ts
@@ -0,0 +1,32 @@
+import { Module, Global } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dirname } from 'path';
+
+import { ConfigService } from '../config/config.service';
+
+@Global()
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        port: configService.get('TYPEORM_PORT'),
+        name: configService.get('TYPEORM_NAME'),
+        host: configService.get('TYPEORM_HOST'),
+        type: configService.get('TYPEORM_CONNECTION'),
+        logging: configService.get('NODE_ENV') === 'development',
+        username: configService.get('TYPEORM_USERNAME'),
+        password: configService.get('TYPEORM_PASSWORD'),
+        database: configService.get('TYPEORM_DATABASE'),
+        dropSchema: configService.get('TYPEORM_DROP_SCHEMA'),
+        synchronize: configService.get('TYPEORM_SYNCHRONIZE'),
+        migrationsRun: configService.get('TYPEORM_MIGRATIONS_RUN'),
+        keepConnectionAlive: configService.get('TYPEORM_KEEP_ALIVE'),
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        entities: [dirname(__dirname) + '/**/*.entity{.ts,.js}'],
+        subscribers: [dirname(__dirname) + '/**/*.subscriber{.ts,.js}'],
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
