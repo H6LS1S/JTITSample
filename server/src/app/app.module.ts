0b73724a74109e7105e9f1fecbae00c206ab75e3
Helsis <seagull.bogdan@gmail.com>
2019-09-22 01:01:49 +0300

    feat( api/decorators ): add user decorator

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -3,10 +3,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '../config/config.module';
 import { DatabaseModule } from '../database/database.module';
 
+import { UserModule } from '../modules/user/user.module';
+
 @Module({
-  imports: [
-    ConfigModule,
-    DatabaseModule,
-  ],
+  imports: [ConfigModule, DatabaseModule, UserModule],
 })
 export class AppModule {}
