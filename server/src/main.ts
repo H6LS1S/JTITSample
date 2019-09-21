06ec8d4c2584b89972851c87180a46ae4549e140
Helsis <seagull.bogdan@gmail.com>
2019-09-21 23:31:49 +0300

    feat( api ): add swagger module

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 import { ConfigService } from './config/config.service';
@@ -8,9 +9,18 @@ import { ConfigService } from './config/config.service';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const config = await new ConfigService();
+  const options = await new DocumentBuilder()
+    .setDescription(config.get('npm_package_description'))
+    .setVersion(config.get('npm_package_version'))
+    .setTitle(config.get('npm_package_name'))
+    .setBasePath(config.get('PREFFIX'))
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('', app, document);
 
   await app
-    .setGlobalPrefix(config.get('API_PREFFIX'))
+    .setGlobalPrefix(config.get('PREFFIX'))
     .useGlobalPipes(new ValidationPipe())
     .enableCors()
     .listen(config.get('PORT'));
