5a3a503e8c00a5eba51517ea705298f4b4e3657b
Helsis <seagull.bogdan@gmail.com>
2019-09-25 19:12:26 +0300

    feat( client/page ): add companies demo page

---
+++ client/pages/companies/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <p />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  layout: 'dashboard',
+})
+export default class CompaniesPage extends Vue {}
+</script>
