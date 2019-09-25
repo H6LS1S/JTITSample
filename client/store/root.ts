542ea0b89565d9fe1f728761fa724f4f00131f3d
Helsis <seagull.bogdan@gmail.com>
2019-09-25 18:46:42 +0300

    feat( client ): add store

---
+++ client/store/root.ts
@@ -0,0 +1,34 @@
+import { GetterTree, ActionTree, MutationTree } from 'vuex';
+import { RootState } from 'store';
+
+export interface State {
+  [x: string]: any;
+}
+
+export const state = (): State => ({
+  pages: [
+    { icon: 'mdi-view-dashboard-outline', attr: { to: '/', exact: true } },
+    { icon: 'mdi-domain', attr: { to: '/companies', exact: true } },
+    {
+      icon: 'mdi-account-group-outline',
+      attr: { to: '/employees', exact: true },
+    },
+    {
+      icon: 'mdi-github-circle',
+      attr: {
+        href: 'https://github.com/HELSIS666/JTITSample',
+        target: '_blank',
+      },
+    },
+  ],
+});
+
+export const getters: GetterTree<RootState, RootState> = {
+  getPages(state) {
+    return state.pages;
+  },
+};
+
+export const actions: ActionTree<RootState, RootState> = {};
+
+export const mutations: MutationTree<RootState> = {};
