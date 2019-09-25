7212900901840eb81cbccaba184a0152391b0985
Helsis <seagull.bogdan@gmail.com>
2019-09-25 19:12:09 +0300

    feat( client/page ): add employees demo page

---
+++ client/pages/employees/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <p />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  layout: 'dashboard',
+})
+export default class EmployeesPage extends Vue {}
+</script>
