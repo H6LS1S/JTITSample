20246206492dac88c79f9b4321fa36cdfc06ef60
Helsis <seagull.bogdan@gmail.com>
2019-09-26 15:27:11 +0300

    feat( client/mixsins ): add download image mixin

--- client/mixins/index.ts
+++ client/mixins/index.ts
@@ -1,5 +1,10 @@
 import Vue from 'vue';
 
 Vue.mixin({
-  methods: {},
+  methods: {
+    getLogotype(id: string) {
+      if (id) return `http://localhost:8081${id}`;
+      return `https://picsum.photos/200/300/?blur`;
+    },
+  },
 });
