c978e2b3ca0ec5cda73df97af5052243661f0b14
Helsis <seagull.bogdan@gmail.com>
2019-09-27 16:04:29 +0300

    chore( api/migration ): refactoring default user

--- server/src/database/migrations/1569103832888-DefaultActiveUser.ts
+++ server/src/database/migrations/1569103832888-DefaultActiveUser.ts
@@ -1,10 +1,13 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
+import { UserEntity } from '../../modules/user/user.entity';
+
 export class DefaultActiveUser1569103832888 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query(
-      `INSERT INTO User(id, email, password, createAt, updateAt) VALUES (DEFAULT, 'admin@admin.com', 'password', DEFAULT, DEFAULT)`,
-    );
+    await queryRunner.connection.getRepository(UserEntity).save({
+      email: 'admin@admin.com',
+      password: 'password',
+    });
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {}
