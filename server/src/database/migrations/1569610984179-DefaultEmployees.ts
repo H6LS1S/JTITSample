f2106a9bc5bebe3c38f3dbaa47248bb630e83e6c
Helsis <seagull.bogdan@gmail.com>
2019-09-27 22:18:18 +0300

    feat( api/migration ): add migration for default employees

---
+++ server/src/database/migrations/1569610984179-DefaultEmployees.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { internet, helpers, name } from 'faker';
+
+import { EmployeeRequestDTO } from '../../modules/employee/dto/employee.dto';
+import { EmployeeEntity } from '../../modules/employee/employee.entity';
+import { CompanyEntity } from '../../modules/company/company.entity';
+
+export class DefaultEmployees1569610984179 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    const companies = await queryRunner.connection
+      .getRepository(CompanyEntity)
+      .find();
+
+    class BaseEmployee implements EmployeeRequestDTO {
+      constructor(company: CompanyEntity) {
+        this.company = company;
+      }
+
+      firstName: string = name.findName();
+      lastName: string = name.lastName();
+      email: string = internet.email();
+      phone: string = '+380' + helpers.replaceSymbolWithNumber('#########');
+      company: CompanyEntity;
+    }
+
+    const employees = companies.map(company => new BaseEmployee(company));
+    return await queryRunner.connection
+      .getRepository(EmployeeEntity)
+      .save(employees);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {}
+}
