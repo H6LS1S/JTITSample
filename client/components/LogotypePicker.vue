0c1c90419f63c02eddb5c3f53a87f9587a012658
Helsis <seagull.bogdan@gmail.com>
2019-09-29 16:32:57 +0300

    feat( client/component ): add logotype picker

---
+++ client/components/LogotypePicker.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-avatar @click="launchFilePicker()" size="144" class="mb-12">
+    <v-img :src="getLogotype(logotype)" />
+    <input
+      ref="file"
+      type="file"
+      accept="image/*"
+      style="display:none"
+      @change="onFileInputChange()"
+    />
+  </v-avatar>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+import { Action } from 'vuex-class';
+
+@Component
+export default class LogotypePicker extends Vue {
+  @Prop({ type: String, required: true }) logotype: string;
+  @Prop({ type: Boolean, required: false }) readonly: string;
+
+  @Action('CompaniesModule/updateCompanyLogotype') updateLogotype;
+
+  private launchFilePicker() {
+    if (!this.readonly) return this.$refs.file.click();
+  }
+
+  private async onFileInputChange(fieldName, file) {
+    const formData = new FormData();
+    const imageFile = this.$refs.file.files[0];
+    const imageURL = URL.createObjectURL(imageFile);
+    formData.append('logotype', imageFile);
+    await this.updateLogotype(formData);
+  }
+}
+</script>
