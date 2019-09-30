0a27bc1b4ee992bb236f1ad49ebcfd690f6273f5
Helsis <seagull.bogdan@gmail.com>
2019-09-30 20:02:56 +0300

    chore( client/mixins ): change image paceholder

--- client/mixins/index.ts
+++ client/mixins/index.ts
@@ -4,7 +4,7 @@ Vue.mixin({
   methods: {
     getLogotype(id: string) {
       if (id) return `http://localhost:8081${id}`;
-      return `https://picsum.photos/200/300/?blur`;
+      return require('../assets/placeholders/404.gif');
     },
   },
 });
